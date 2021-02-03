a7a5eb44637cb3c408c609d63a06ab0f8be8a252
oscarlr <olrodriguez01@gmail.com>
2021-02-03 12:23:30 -0500

    add

---
+++ parse_imgt.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+import sys
+
+# File should be from IMGT_V-QUEST_reference_directory
+infile = sys.argv[1]
+
+with open(infile, 'r') as fh:
+    for line in fh:
+        line = line.rstrip()
+        seqs = []
+        if ">" in line:
+            if len(seqs) != 0:
+                print(">%s" % name)
+                print("%s" % "".join(seqs))
+                seqs = []
+            line = line.split('|')
+            gene = line[1].split("*")[0]
+            allele = line[1].split("*")[1]
+            function = line[3]
+            name = "gene=%s_allele=%s_function=%s" % (gene,allele,function)
+        else:
+            seqs.append(line.replace(".",''))
+            
+print(">%s" % name)
+print("%s" % "".join(seqs))
+
+
